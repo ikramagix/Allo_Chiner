3af536dab0185c131b2a136c4672d8b5557e669e
ikramiste <ikrame.saadi@gmail.com>
2024-01-26 09:44:33 +0100

    API back online live doing researchs

--- scripts.js
+++ scripts.js
@@ -1,108 +1,88 @@
-$(document).ready(function() {
-    let API_KEY;
-
-    // Désactiver le bouton de recherche initialement
-    $('#searchButton').prop('disabled', true);
-
-    // Récupérer la clé API
-    $.get("config.php", function(data) {
-        API_KEY = data.API_KEY;
-
-        // Activer le bouton de recherche
-        $('#searchButton').prop('disabled', false);
-
-        // Afficher un message indiquant que la recherche est prête
-        $('#searchStatus').text("La recherche est maintenant activée.");
+$(document).ready(function () {
+    $("#searchForm").on("submit", function (e) {
+      e.preventDefault();
+      let searchText = $("#searchText").val();
+      fetchMovies(searchText);
     });
-
-    // Gestion de la soumission du formulaire de recherche
-    $('#searchForm').on('submit', function(e) {
-        e.preventDefault();
-        if (!API_KEY) {
-            alert("La recherche n'est pas encore disponible. Veuillez réessayer dans un instant.");
-            return;
-        }
-        const searchText = $('#searchText').val();
-        fetchMovies(searchText);
+  
+    $(document).ready(function () {
+      animateMovies();
     });
-      // Gestion de la soumission du formulaire de recherche
-      $('#searchForm').on('submit', function(e) {
-          e.preventDefault();
-          const searchText = $('#searchText').val();
-          fetchMovies(searchText);
+  
+    function fetchMovies(searchText) {
+      $.ajax({
+        method: "GET",
+        url: `http://www.omdbapi.com/?s=${searchText}&apikey=834bdcbd`,
+        dataType: "json",
+        success: onMoviesReceived,
+        error: function () {
+          alert("Erreur lors de la récupération des données.");
+        },
       });
-
-      // Récupération des films depuis l'API OMDb
-      function fetchMovies(searchText) {
-          $.ajax({
-              method: 'GET',
-              url: `http://www.omdbapi.com/?i=tt1630029&apikey=${API_KEY}`,
-              dataType: 'json',
-              success: function(data) {
-                  if (data.Response === 'True') {
-                      updateGallery(data.Search);
-                  } else {
-                      $('#movieGallery').html('<div class="col">Film non trouvé.</div>');
-                  }
-              },
-              error: function() {
-                  alert('Erreur lors de la récupération des données.');
-              },
-          });
+    }
+  
+    function onMoviesReceived(response) {
+      if (response.Response === "True") {
+        displayMovies(response.Search);
+      } else {
+        $("#movies").html('<div class="col">Film non trouvé.</div>');
       }
-
-      // Mise à jour de la galerie de films
-      function updateGallery(movies) {
-          let galleryHtml = '';
-          movies.forEach(function(movie) {
-              galleryHtml += `
-                  <div class="col-xl-3 col-lg-4 col-md-6">
-                      <div class="gallery-item h-100">
-                          <img src="${movie.Poster}" class="img-fluid" alt="${movie.Title}">
-                          <div class="gallery-links d-flex align-items-center justify-content-center">
-                              <a href="${movie.Poster}" title="${movie.Title}" class="glightbox preview-link"><i class="bi bi-arrows-angle-expand"></i></a>
-                              <a href="#" class="details-link read-more" data-id="${movie.imdbID}"><i class="bi bi-link-45deg"></i> En savoir plus</a>
-                          </div>
-                      </div>
-                  </div>`;
-          });
-          $('#movieGallery').html(galleryHtml);
-      }
-
-      // Gestion des clics sur "En savoir plus" pour récupérer plus de détails
-      $(document).on('click', '.read-more', function() {
-          const movieId = $(this).data('id');
-          fetchMovieDetails(movieId);
+    }
+  
+    function displayMovies(movies) {
+      const template = $("#movie-template").html();
+      movies.forEach((movie) => {
+        let movieHtml = template
+          .replace("{{id}}", movie.imdbID)
+          .replace("{{poster}}", movie.Poster)
+          .replace("{{title}}", movie.Title)
+          .replace("{{release_date}}", movie.Year);
+        $("#movies").append(movieHtml);
       });
-
-      // Récupération des détails d'un film spécifique
-      function fetchMovieDetails(movieId) {
-          $.ajax({
-              method: 'GET',
-              url: `https://www.omdbapi.com/?i=${movieId}&apikey=${API_KEY}`,
-
-              dataType: 'json',
-              success: function(response) {
-                  displayMovieDetails(response);
-              },
-              error: function() {
-                  alert('Erreur lors de la récupération des détails du film.');
-              },
-          });
-      }
-
-      // Affichage des détails d'un film
-      function displayMovieDetails(details) {
-          const detailsHtml = `
-              <strong>Date de sortie:</strong> ${details.Released}<br>
-              <strong>Genre:</strong> ${details.Genre}<br>
-              <strong>Scénario:</strong> ${details.Plot}`;
-          $('.modal-body').html(detailsHtml);
-          $('#movieModal').modal('show');
-      }
-
-      // Fermeture de la modale avec le bouton
-      $(document).on('click', '.close[data-dismiss="modal"]', function() {
-          $('#movieModal').modal('hide');
+      animateMovies();
+    }
+  
+    function animateMovies() {
+      const movies = $(".movie-card");
+      const windowHeight = $(window).height();
+  
+      $(window).scroll(function () {
+        const windowTop = $(window).scrollTop();
+  
+        movies.each(function () {
+          const movieTop = $(this).offset().top;
+  
+          if (windowTop + windowHeight > movieTop) {
+            $(this).addClass("visible");
+          }
+        });
       });
+    }
+  
+    $(document).on("click", ".read-more", function () {
+      let movieId = $(this).data("id");
+      fetchMovieDetails(movieId);
+    });
+  
+    function fetchMovieDetails(movieId) {
+      $.ajax({
+        method: "GET",
+        url: `http://www.omdbapi.com/?i=${movieId}&apikey=834bdcbd`,
+        dataType: "json",
+        success: function (response) {
+          displayMovieDetails(response);
+        },
+        error: function () {
+          alert("Erreur lors de la récupération des détails du film.");
+        },
+      });
+    }
+  
+    function displayMovieDetails(details) {
+      let detailsHtml = `<strong>Date de sortie:</strong> ${details.Released}<br>
+                             <strong>Genre:</strong> ${details.Genre}<br>
+                             <strong>Scénario:</strong> ${details.Plot}`;
+      $(".modal-body").html(detailsHtml);
+      $("#movieModal").modal("show");
+    }
   });
